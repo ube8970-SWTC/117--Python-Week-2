c41b95909bac89ac18c677f830a26629a28e2efb
Dylan Ubersox <ube8970@swtc.edu>
2026-08-24 12:16:05 -0500

    First Commit

---
+++ Week_2_Task_1.py
@@ -0,0 +1,12 @@
+print("\nWeek 2 Task 1: Weather Check\n")
+Temperature = 71
+# 71 is the current temp at time of code creation
+
+print("Temperature:", Temperature)
+
+if Temperature >= 70:
+    Print("It's hot")
+elif Temperature >= 55:
+    Print("It's warm")
+elif Temperature >= 32:
+    Print("It's Cold!")
