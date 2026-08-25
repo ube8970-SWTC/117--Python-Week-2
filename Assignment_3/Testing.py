5dacdb6118edd6abe23b026d2bca1f799ab878c4
Dylan Ubersox <ube8970@swtc.edu>
2026-08-25 11:48:25 -0500

    First commit

---
+++ Assignment_3/Testing.py
@@ -0,0 +1,7 @@
+print("counting from 1 to 25:")
+
+
+for number in range(1, 26):  # Loop through numbers from 1 to 25
+    print(number)  # Print the current number in the loop
+    
+print("Counting complete!")  # Indicate that counting is finished
